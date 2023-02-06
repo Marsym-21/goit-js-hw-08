a67e4cd89cc7b1d80a242be33cc233a455706fd9
Marsym-21 <mini.garnett.21@gmail.com>
2023-02-06 12:06:01 +0100

    111

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -3,3 +3,28 @@ import { galleryItems } from './gallery-items';
 // Change code below this line
 
 console.log(galleryItems);
+
+import SimpleLightbox from 'simplelightbox';
+
+import 'simplelightbox/dist/simple-lightbox.min.css';
+
+const gallery = document.querySelector('.gallery');
+
+const cardsMarkup = createGalleryImages(galleryItems);
+
+gallery.insertAdjacentHTML('beforeend', cardsMarkup);
+
+function createGalleryImages(items) {
+  return items
+    .map(({ preview, original, description }) => {
+      return `<a class="gallery__item" href="${original}">
+						<img class="gallery__image" src="${preview}" data-source="${original}" alt="${description}" />
+					</a>`;
+    })
+    .join(``);
+}
+
+lightbox = new SimpleLightbox('.gallery a', {
+  captionsData: 'alt',
+  captionDelay: 250,
+});
