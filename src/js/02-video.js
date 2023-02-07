303a0bfb0f86e0836b8d8d3e6f1ba964286cfcfd
Marsym-21 <mini.garnett.21@gmail.com>
2023-02-07 20:07:07 +0100

    123

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,10 +1,12 @@
 import Player from '@vimeo/player';
 
-const player = new Player('handstick', {
+let player = new Player('handstick', {
   id: 19231868,
   width: 640,
 });
 
-player.on('play', function () {
-  console.log('played the video!');
-});
+player.on('play', onPlay);
+
+const onPlay = function (data) {
+  console.log(data);
+};
