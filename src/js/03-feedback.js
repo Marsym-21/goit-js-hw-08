473f4b0c7847a171ab62d0b7327a743db24a926b
Marsym-21 <mini.garnett.21@gmail.com>
2023-02-08 01:15:46 +0100

    Update 03-feedback.js

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,19 @@
 const throttle = require('lodash.throttle');
+const form = document.querySelector('.feedback-form');
+form.addEventListener('submit', event => {
+  event.preventDefault();
+  const {
+    elements: { email, message },
+  } = event.currentTarget;
+
+  const newObjectsData = {
+    Email: email.value,
+    Message: message.value,
+  };
+
+  localStorage.setItem('feedback-form-state', JSON.stringify(newObjectsData));
+
+  console.log(localStorage.getItem('feedback-form-state'));
+
+  event.currentTarget.reset();
+});
