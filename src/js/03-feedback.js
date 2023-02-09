8cae747b984d1a34378443c5aa0e22f376a4abb3
Marsym-21 <mini.garnett.21@gmail.com>
2023-02-09 20:02:20 +0100

    123

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,4 +1,5 @@
-import throttle from 'lodash.throttle';
+// import throttle from 'lodash.throttle';
+const throttle = require('lodash.throttle');
 const form = document.querySelector('.feedback-form');
 const STORAGE_KEY = 'feedback-form-state';
 
