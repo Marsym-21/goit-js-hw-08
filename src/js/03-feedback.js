27a59d9eda9c129b12ea5f63657caebeace8786f
Marsym-21 <mini.garnett.21@gmail.com>
2023-02-09 14:18:21 +0100

    Update 03-feedback.js

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,19 +1,45 @@
 const throttle = require('lodash.throttle');
 const form = document.querySelector('.feedback-form');
+
+form.addEventListener(
+  'input',
+  throttle(event => {
+    const {
+      elements: { email, message },
+    } = event.currentTarget;
+
+    const newObjectsData = {
+      Email: email.value,
+      Message: message.value,
+    };
+
+    localStorage.setItem('feedback-form-state', JSON.stringify(newObjectsData));
+
+    const savedData = localStorage.getItem('feedback-form-state');
+    console.log(savedData);
+
+    const savedDataArray = JSON.parse(savedData);
+
+    if (savedDataArray.Email !== '') {
+      console.log('Заповнена Строка');
+      email.value = savedDataArray.Email;
+      //   message.value = savedDataArray.Message;
+    }
+  }, 500)
+);
+
 form.addEventListener('submit', event => {
   event.preventDefault();
   const {
     elements: { email, message },
   } = event.currentTarget;
 
-  const newObjectsData = {
+  const objectsData = {
     Email: email.value,
     Message: message.value,
   };
 
-  localStorage.setItem('feedback-form-state', JSON.stringify(newObjectsData));
-
-  console.log(localStorage.getItem('feedback-form-state'));
-
-  event.currentTarget.reset();
+  localStorage.removeItem('feedback-form-state');
+  console.log(objectsData);
+  event.currentTarget.reset;
 });
