7c5cf3302b39bcb133294fa7b15dc69a17e2bb63
Marsym-21 <mini.garnett.21@gmail.com>
2023-02-09 15:50:39 +0100

    123

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,48 +1,31 @@
 import throttle from 'lodash.throttle';
 const form = document.querySelector('.feedback-form');
 const STORAGE_KEY = 'feedback-form-state';
+const formData = {};
 
 form.addEventListener(
   'input',
   throttle(event => {
-    const {
-      elements: { email, message },
-    } = event.currentTarget;
-
-    const newObjectsData = {
-      Email: email.value,
-      Message: message.value,
-    };
-
-    localStorage.setItem(STORAGE_KEY, JSON.stringify(newObjectsData));
+    formData[event.target.name] = event.target.value;
+    localStorage.setItem(STORAGE_KEY, JSON.stringify(formData));
 
     const savedData = localStorage.getItem(STORAGE_KEY);
-    console.log(savedData);
+    const newFormData = JSON.parse(savedData);
 
-    const savedDataArray = JSON.parse(savedData);
-
-    if (savedDataArray.Email !== '') {
-      console.log('Заповнена Строка');
-      email.value = savedDataArray.Email;
-      //   message.value = savedDataArray.Message;
+    if (savedData) {
+      console.log('Є запис');
+      for (key in newFormData) {
+        if (key === event.target.name) {
+          event.target.value = newFormData[key];
+        }
+      }
     }
   }, 500)
 );
 
 form.addEventListener('submit', event => {
   event.preventDefault();
-
-  const {
-    elements: { email, message },
-  } = event.currentTarget;
-
-  const objectsData = {
-    Email: email.value,
-    Message: message.value,
-  };
-
   localStorage.removeItem(STORAGE_KEY);
-  console.log(objectsData);
-
-  event.currentTarget.reset();
+  console.log(formData);
+  event.target.reset();
 });
