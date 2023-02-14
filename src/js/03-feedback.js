c03dc55aa46a34b1552d7a21276c98139a7e4c98
Marsym-21 <mini.garnett.21@gmail.com>
2023-02-14 11:25:36 +0100

    Update 03-feedback.js

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -5,20 +5,34 @@ const STORAGE_KEY = 'feedback-form-state';
 
 populateText();
 const formData = {};
-
 form.addEventListener('input', throttle(checkedInputValue, 500));
 
 function checkedInputValue(event) {
+  const { email, message } = form.elements;
+
   const name = event.target.name;
   const value = event.target.value;
   formData[name] = value;
+  console.log(formData);
+
+  if (email.value !== '') {
+    console.log(email.value);
+  }
+  if (message.value !== '') {
+    console.log(message.value);
+  }
+
   localStorage.setItem(STORAGE_KEY, JSON.stringify(formData));
 }
 
 form.addEventListener('submit', event => {
   event.preventDefault();
+  const { email, message } = form.elements;
+
+  if (email.value === '' || message.value === '') {
+    return alert('Please fill in all the fields!');
+  }
   localStorage.removeItem(STORAGE_KEY);
-  console.log(formData);
   event.target.reset();
 });
 
@@ -26,7 +40,6 @@ function populateText() {
   const savedData = localStorage.getItem(STORAGE_KEY);
   const newFormData = JSON.parse(savedData);
   if (newFormData) {
-    console.log('Є запис');
     const { email, message } = form.elements;
     email.value = newFormData.email || '';
     message.value = newFormData.message || '';
